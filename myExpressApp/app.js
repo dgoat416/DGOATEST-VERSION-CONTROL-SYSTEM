6d75dc59c06d8dba79ff33b6f93116011b7078de
dgoat416 <55709445+dgoat416@users.noreply.github.com>
2020-09-20 23:18:40 -0700

    Update DGOAT's VCS system
    - Really messy code but this is my first time programming using this tech stack so I had to try a lot of things out to see if they work or not
    *** WILL FIX THIS LATER ****
    - Implemented the "snapshot" feature - Got the front end up and running almost optimally (no frills)
    - Able to receive input from user to create a repository
    
    Left to Implement:
    - Manifest files
    - Figure out how to distinguish between snapshots
    - Etc.

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -51,57 +51,130 @@
 
 // console.log("You're GOATED");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // File: app.js
 // Load the Express builder functions
-const express = require('express');
+// const express = require('express');
 const path = require('path');
+// const bodyParser = require('body-parser')
+var fs = require('fs');
+const { raw } = require('express');
 
-// Init an Express object
-const app = express();
+// // Init an Express object
+// const app = express();
 
 // Load View Engine 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+// app.use(bodyParser.urlencoded({extended : true}));
 
-// Set page-gen fcn for URL root request
-app.get('/', function (request, response) 
-{ 
-  let articles = [
-    {
-      id: 1, 
-      title: 'Article One',
-      author: "Deron Washington II",
-      body: 'This is article one'
-    },
-    {
-      id: 2, 
-      title: 'Article Two',
-      author: "DGOAT",
-      body: 'This is article two'
-    },
-    {
-      id: 3, 
-      title: 'Article Three',
-      author: "DFAT",
-      body: 'This is article three'
-    }
-  ];
+// // Set page-gen fcn for URL root request
+// app.get('/', function (request, response) 
+// { 
+//    return response.render('index', {
+//       title: 'TeamDJ Version Control System (v1.0.0)'
+//     });
+// });
 
-    response.render('index', {
-      title: 'TeamDJ Version Control System (v1.0.0)',
-      articles: articles
-    });
-});
+
+// app.get('/submit-repo-info', function(request, response){
+
+// navigate to a new page
+// response.render('submit-repo-info', {
+//   result: 'Your repository has been created from ' + currentProjectDir + ' and stored '
+//   + newRepositoryDir + ' and named ' + 'myFirstRepo'
+// });
+
+// });
+
+
+// app.use(express.static('/'));
+// // handle a client side action request
+// app.get('/get-form-text', function(request, response){
+//   // gives me the current project directory to take snapshot from
+//   var currentProjectDir = request.query.currProjectDir;
+
+//   // gives me the directory where the repository will be stored
+//   var newRepositoryDir = request.query.newRepoDir;
+
+//   // output our text to the console
+//   console.log('app.js received = ' + currentProjectDir + ' for ' + newRepositoryDir);
+  
+//   // send our text back to the client side within this form
+//   response.send('Your Text: ' + currentProjectDir + ' for ' + newRepositoryDir);
+
+
+//   // create the repository
+// var files = fs.readdirSync(currentProjectDir);
+// console.log(files);
+//   // path.resolve()
+
+// });
 
 // Add Route
-app.get('/articles/add', function (req, res){
-  res.render('add_article', {
-    title: 'add OTIS'
-  });
-});
+// app.get('/articles/add', function (req, res){
+//   res.render('add_article', {
+//     title: 'add OTIS'
+//   });
+// });
 
-// Start Server
-app.listen(3000, function (req, res) 
-{ 
-  console.log('app.js listening on port 3000!');
-}); // https://discord.com/channels/738205874929270815/738205874929270818/756227895478845556 
+// // Start Server
+// app.listen(3000, function (req, res) 
+// { 
+//   console.log('app.js listening on port 3000!');
+// }); // https://discord.com/channels/738205874929270815/738205874929270818/756227895478845556 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Method to take a snapshot of a directory
+@param source = source directory to copy from
+@param dest   = destination directory to copy source to
+@param callback = callback method
+*/
+function snapshot(source, dest, callback)
+{
+  // import recursive-copy package
+  var copy = require('recursive-copy');
+
+  // perform a copy of the source directory
+  // copy(source, destination, callback)
+  copy(source, dest, function(error, results) {
+      if (error) {
+          console.error('Copy failed: ' + error);
+      } else {
+          console.log(results);
+          console.info('Copied ' + results.length - 1 + ' files');
+      }
+  });
+}
+
+
+var currPath = 'C:\\Users\\HP\\Documents\\Current Classes\\PSY 150'//'C:\\Users\\HP\\Documents\\JS Test Folder';
+var repoPath = 'C:\\Users\\HP\\Documents\\Repos\\Repo1';
+
+// create the repository  
+snapshot(currPath, repoPath);
